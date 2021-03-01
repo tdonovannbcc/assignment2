27f621d823fea4aba8da44e4f4e67c9759d09ea6
tdonovannbcc <trevordonovan.nbcc@gmail.com>
2021-02-28 20:20:28 -0400

    update

--- app/src/main/java/work/nbcc/rick_and_morty_quiz/GameViewModel.kt
+++ app/src/main/java/work/nbcc/rick_and_morty_quiz/GameViewModel.kt
@@ -94,24 +94,24 @@ class GameViewModel : ViewModel() {
         questions = mutableListOf(
                 Question(R.string.question_1, false),
                 Question(R.string.question_2, true),
-                Question(R.string.question_3, true)
-//                Question(R.string.question_4, false),
-//                Question(R.string.question_5, false),
-//                Question(R.string.question_6, true),
-//                Question(R.string.question_7, false),
-//                Question(R.string.question_8, true),
-//                Question(R.string.question_9, false),
-//                Question(R.string.question_10, false),
-//                Question(R.string.question_11, false),
-//                Question(R.string.question_12, true),
-//                Question(R.string.question_13, false),
-//                Question(R.string.question_14, true),
-//                Question(R.string.question_15, false),
-//                Question(R.string.question_16, false),
-//                Question(R.string.question_17, true),
-//                Question(R.string.question_18, false),
-//                Question(R.string.question_19, false),
-//                Question(R.string.question_20, true)
+                Question(R.string.question_3, true),
+                Question(R.string.question_4, false),
+                Question(R.string.question_5, false),
+                Question(R.string.question_6, true),
+                Question(R.string.question_7, false),
+                Question(R.string.question_8, true),
+                Question(R.string.question_9, false),
+                Question(R.string.question_10, false),
+                Question(R.string.question_11, false),
+                Question(R.string.question_12, true),
+                Question(R.string.question_13, false),
+                Question(R.string.question_14, true),
+                Question(R.string.question_15, false),
+                Question(R.string.question_16, false),
+                Question(R.string.question_17, true),
+                Question(R.string.question_18, false),
+                Question(R.string.question_19, false),
+                Question(R.string.question_20, true)
         )
 
         questions.shuffle()
